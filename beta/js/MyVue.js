fb9532fae3de0c699993a1f095e752c766160071
Zhassulan <zhassulan.baigozha2@gmail.com>
2019-10-17 00:28:43 +0600

    console.log(localStorage.getItem("lang"));

--- beta/js/MyVue.js
+++ beta/js/MyVue.js
@@ -6,8 +6,10 @@ new Vue({
     methods: {
         langFunc () {
             if(localStorage.getItem("lang") === null){
+                console.log(localStorage.getItem("lang"));
                 this.message = localStorage.getItem("lang");
             } else{
+                console.log(localStorage.getItem("lang"));
                 localStorage.setItem("lang", "RU");
                 this.message = localStorage.getItem("lang");
             };
