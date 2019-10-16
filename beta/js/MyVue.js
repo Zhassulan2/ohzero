78ac7e8eb08dcc39c819425b4fa7f786b79338e5
Zhassulan <zhassulan.baigozha2@gmail.com>
2019-10-17 00:48:36 +0600

    <script src="./js/MyVue.js?dev=8"></script>

--- beta/js/MyVue.js
+++ beta/js/MyVue.js
@@ -6,14 +6,14 @@ new Vue({
     methods: {
 
         langFunc () {
-            /*if(localStorage.getItem("lang") === null){
+            if(localStorage.getItem("lang") === null){
                 console.log(localStorage.getItem("lang"));
                 this.message = localStorage.getItem("lang");
             } else{
                 console.log(localStorage.getItem("lang"));
                 localStorage.setItem("lang", "RU");
                 this.message = localStorage.getItem("lang");
-            };*/
+            };
         }
     },
     mounted() {
