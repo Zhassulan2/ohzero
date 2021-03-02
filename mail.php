5c7046725bf678511a57eb924cc0fb9749de5ec6
Zhassulan <zhassulan.baigozha2@gmail.com>
2021-03-02 18:58:01 +0600

    added new line

--- mail.php
+++ mail.php
@@ -3,8 +3,8 @@
 $to = 'zhassulan.baigozha2@gmail.com'; // адрес получателя
 $subject = 'Заказ обратного звонка'; // тема письма
 $message = "Имя: {$_POST['name']}\r\n"; // добавляем имя в текст
-$message .= "Телефон: {$_POST['phone']}"; // добавляем телефон в текст
-$message .= "Email: {$_POST['email']}"; // добавляем телефон в текст
+$message .= "Телефон: {$_POST['phone']}\r\n"; // добавляем телефон в текст
+$message .= "Email: {$_POST['email']}\r\n"; // добавляем телефон в текст
 $headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n"; // установливаем кодировку
 $headers .= 'From: mail@ohzero.kz' . "\r\n"; // добавляем отправителя
 if( mail($to, $subject, $message, $headers) ){
